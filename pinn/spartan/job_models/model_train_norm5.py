64c1c89718fe6462e2c94ad561f93aae2862bcb2
radhikrammohan <radhikrammohan@gmail.com>
2025-06-07 16:52:03 +1000

    test again

--- pinn/spartan/job_models/model_train_norm5.py
+++ pinn/spartan/job_models/model_train_norm5.py
@@ -34,7 +34,7 @@ from Model.loss_func_sol_norm import loss_fn_data,pde_loss,ic_loss,boundary_loss
 from Model.train_testloop_norm import *
 
 
-settings_path_1 = os.path.join(current_dir, '..', 'training_data', 'settings5.json')
+settings_path_1 = os.path.join(current_dir, '../..', 'training_data', 'settings5.json')
 with open(settings_path_1,'r') as file:
     settings = json.load(file)
 
@@ -48,7 +48,7 @@ dt = heat_data.dt
 dx = heat_data.dx
 # print(heat_data.dx)
 # print(dt)
-settings_path = os.path.join(current_dir, '..', 'training_data', 'settings5.json')
+settings_path = os.path.join(current_dir, '../..', 'training_data', 'settings5.json')
 with open(settings_path,'r') as file:
     props = json.load(file)
     
